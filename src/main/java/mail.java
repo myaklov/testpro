c41f7503977160a9ba520af876206585f60c9835
Alexandr Myaklov <awm001@yandex.ru>
2017-06-29 17:46:09 +0700

    test project

---
+++ src/main/java/mail.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Alex on 29.06.2017.
+ */
+public class mail {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("test");
+    }
+}
